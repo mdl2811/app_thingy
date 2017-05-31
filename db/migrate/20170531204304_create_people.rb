158a2375a028abdf4f7e029b452c9260443c732e
Michael LaBahn <mdlabahn@yahoo.com>
2017-05-31 15:55:57 -0600

    working

--- db/migrate/20170531204304_create_people.rb
+++ db/migrate/20170531204304_create_people.rb
@@ -1,11 +1,12 @@
-class CreateNames < ActiveRecord::Migration[5.1]
+class CreatePeople < ActiveRecord::Migration[5.1]
   def change
-    create_table :names do |t|
+    create_table :people do |t|
+      t.string :name
       t.integer :age
       t.string :hair_color
       t.string :eye_color
       t.string :gender
-      t.boleon :alive
+      t.boolean :alive
 
       t.timestamps
     end
